0b2b5a2896871aa7c5c1bf7a7a4eedb0d6509025
ksheer506 <ksheer506@naver.com>
2025-12-18 10:40:42 +0900

    fix: 비밀번호 조건 간소화

--- front/services/user/schema.ts
+++ front/services/user/schema.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { hasAlphabet, hasNumber, hasSpecial } from "@/lib/validate";
+import { hasAlphabet, hasNumber } from "@/lib/validate";
 
 export const SignUpSchema = z.object({
   email: z.email({ error: "이메일이 형식에 맞지 않습니다." }),
@@ -12,9 +12,6 @@ export const SignUpSchema = z.object({
     .nonempty({ error: "비밀번호을 입력해주세요." })
     .min(6, { error: "비밀번호는 6자 이상이어야 합니다." })
     .superRefine((v, ctx) => {
-      if (hasSpecial(v) && hasAlphabet(v) && hasNumber(v) && v.length < 6) {
-        return ctx.addIssue("특수문자+영문+숫자 비밀번호인 경우 6자 이상이어야 합니다.")
-      }
       if (hasAlphabet(v) && hasNumber(v) && v.length < 8) {
         return ctx.addIssue("영문+숫자 비밀번호인 경우  8자 이상이어야 합니다.")
       }
