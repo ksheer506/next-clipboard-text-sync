dcfc43862cc07b6be7a4caf80de6aaa8eb0ae25e
ksheer506 <ksheer506@naver.com>
2026-01-10 17:13:07 +0900

    feat: 랜딩 페이지 추가

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
 import { SessionProvider } from "next-auth/react";
+import { Theme } from "@radix-ui/themes";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Header />
         <SessionProvider>
-          {children}
+          <Theme accentColor="cyan">
+            <main className="w-screen h-screen geometric-bg">
+              {children}
+            </main>
+          </Theme>
         </SessionProvider>
       </body>
     </html>
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,13 +1,13 @@
+import Landing from "@/components/Landing";
 import { ROUTE } from "@/const/route";
 import { getSession } from "@/server-actions/auth";
 import Link from "next/link";
-import { redirect, RedirectType } from "next/navigation";
 
 export default async function Home() {
   const session = await getSession();
 
   if (!session) {
-    redirect(ROUTE.AUTH.SIGN_IN, RedirectType.replace);
+    return <Landing />
   }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
