22b5fd64387ba4283a09ed4614be5b7370a0e075
ksheer506 <ksheer506@naver.com>
2026-01-19 10:26:52 +0900

    feat: 로딩 스피너 추가

---
+++ front/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="bg-transparent-background w-full h-full flex items-center justify-center">
+      <span className="loader" />
+    </div>
+  )
+}
